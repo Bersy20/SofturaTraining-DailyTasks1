a5e32765230808061060f0c357d8522e330a61ff
Bersy S B <bersysilvesterbatrish@outlook.com>
2021-06-18 11:18:40 +0530

    Task

--- June18_Activity/DropDownListTask/Models/DeptModel.cs
+++ June18_Activity/DropDownListTask/Models/DeptModel.cs
@@ -22,23 +22,19 @@ namespace DropDownListTask.Models
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.CommandText = " select distinct Name from HumanResources.Department ";
             cmd.Connection = con;
-            con.Open();
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt);
-            con.Close();
             return dt;
         }
         public DataTable GetEmpByDept(string DeptName)
         {
             DataTable dt = new DataTable();
             SqlConnection con = new SqlConnection("data source=.;database=AdventureWorks2019;Integrated Security=true");
-            con.Open();
             SqlCommand cmd = new SqlCommand("sp_GetEmpByDept", con);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@Name",DeptName);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt);
-            con.Close();
             return dt;
         }
     }
